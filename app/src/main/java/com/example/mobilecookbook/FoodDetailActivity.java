a020110500b7347065e95ac452ed16470bbb1c40
Ekmot45 <128604823+Ekmot45@users.noreply.github.com>
2023-06-08 14:59:27 +0200

    add improving the action bar and adding messages

--- app/src/main/java/com/example/mobilecookbook/FoodDetailActivity.java
+++ app/src/main/java/com/example/mobilecookbook/FoodDetailActivity.java
@@ -42,7 +42,7 @@ public class FoodDetailActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.details_menu, menu);
         return true;
     }
 
